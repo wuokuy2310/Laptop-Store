205c776b1031d428df6f135b48dea4e8714b78f0
wuokuy2310 <uyhotboy112@gmail.com>
2025-06-13 18:52:03 +0700

    LaptopStore

---
+++ src/main/java/com/ecom/service/CommnServiceImpl.java
@@ -0,0 +1,31 @@
+package com.ecom.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+@Service
+public class CommnServiceImpl implements CommonService {
+
+	@Value("${rupee.sign}")
+	public String rupeeSign;
+	
+	@Override
+	public void removeSessionMessage() {
+		HttpServletRequest request = ((ServletRequestAttributes) (RequestContextHolder.getRequestAttributes()))
+				.getRequest();
+		HttpSession session = request.getSession();
+		session.removeAttribute("succMsg");
+		session.removeAttribute("errorMsg");
+	}
+	
+	@Override
+	public String rupeeSign()
+	{
+		return rupeeSign;
+	}
+}
